42e7614001d74c48fd5af381a086552e7addbf40
Nivedita Mandal <51962570+NiveditaMandal@users.noreply.github.com>
2022-06-11 03:04:28 +0530

    Update and rename Q16_Palindrome.java to Q16_Reverse_A_String.java

---
+++ Q16_Reverse_A_String.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q16_Reverse_A_String {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = in.nextLine();
+        System.out.println("Reverse of a string "+str+" is: ");
+        int i = str.length();
+        while(i>0){
+            System.out.print(str.charAt(i-1));
+            i--;
+        }
+
+    }
+}
