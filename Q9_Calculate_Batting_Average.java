367f69171ef465c3804b885d6739abc8b3f76de6
Nivedita Mandal <51962570+NiveditaMandal@users.noreply.github.com>
2022-06-11 02:17:26 +0530

    Create Q9_Calculate_Batting_Average.java

---
+++ Q9_Calculate_Batting_Average.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Q9_Calculate_Batting_Average {
+    public static void main(String[] args){
+        // Calculate Batting Average
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of Runs: ");
+        int runs = input.nextInt();
+        System.out.println("Enter number of matches");
+        int matches=input.nextInt();
+        System.out.println("Enter number of not-out matches");
+        int notout=input.nextInt();
+        int outmatches=matches-notout;
+        double baverage=runs/outmatches;
+        System.out.println("Batting Average= "+baverage);
+
+    }
+}
